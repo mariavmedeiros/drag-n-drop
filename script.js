cc8be5a65a89747023f12fa97133d9eac9508229
Maria Medeiros <maitevelori@hotmail.com>
2022-12-19 16:54:51 -0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelector(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleve", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+function dragStart() {
+  console.log("drag start");
+}
+
+function dragEnd() {
+  console.log("drag end");
+}
+
+function dragOver() {
+  console.log("drag over");
+}
+
+function dragEnter() {
+  console.log("drag enter");
+}
+
+function dragLeave() {
+  console.log("drag leave");
+}
+
+function dragDrop() {
+  console.log("drag drop");
+}
